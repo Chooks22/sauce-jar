d74eb4936e3b0d6c346abca071bbc3e599faf503
Choooks22 <49726759+Choooks22@users.noreply.github.com>
2022-05-19 10:31:59 +0800

    chore: add basic diagnostics

--- events/ready.ts
+++ events/ready.ts
@@ -3,7 +3,10 @@ import { defineEvent } from 'chooksie'
 export default defineEvent({
   name: 'ready',
   once: true,
-  execute(ctx) {
-    ctx.logger.info(`Logged in as ${ctx.client.user.username}!`)
+  execute(ctx, client) {
+    ctx.logger.info(`Logged in as ${client.user.username}!`)
+    const guilds = client.guilds.cache
+    const memberCount = guilds.reduce((total, guild) => total + guild.memberCount, 0)
+    ctx.logger.info(`Guilds: ${guilds.size}; Members: ${memberCount}`)
   },
 })
